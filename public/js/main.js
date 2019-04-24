8b33f07fbbd9c28e4004c5ce788fe33fdb1cc499
Jesus Cebader <zebader@hotmail.com>
2019-04-25 01:11:19 +0200

    about gallery done

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,10 @@
 
 const navElem = document.querySelector('nav');
 const colorArr = ['#b91e28','#ce414a','#e21d2a'];
-const sectionH2Elem = document.querySelector('main section h2');
+const sectionH2Elem = document.querySelector('main section h2.h2effect');
 const fontSizeArr = ['2em','4em','6em'];
 const fontFamArr = ['ZCOOL KuaiLe','Poppins'];
+const footer = document.querySelector('footer');
 
 let colorEffect = setInterval(function(){
 
@@ -12,11 +13,18 @@ let colorEffect = setInterval(function(){
   navElem.style.background = colorArr[randomColor];
 },100);
 let randowmH2Effect = setInterval(function(){
-
+  if(sectionH2Elem !== null){
   const randomFontSize = Math.floor(Math.random()*fontSizeArr.length);
   sectionH2Elem.style.fontSize = fontSizeArr[randomFontSize];
 
   const randomFontFam = Math.floor(Math.random()*fontFamArr.length);
   sectionH2Elem.style.fontFamily = fontFamArr[randomFontFam];
-
+  }
 },100);
+
+footer.addEventListener('mouseenter',function(){
+  this.style.background ='url(../img/nick-footer-2.gif) center'
+})
+footer.addEventListener('mouseout',function(){
+  this.style.background ='url(../img/nick-footer.gif) center'
+})
